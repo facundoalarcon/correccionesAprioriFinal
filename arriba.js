da5629f7ace2a1e197b92205f920fcba69c6fcad
facundoalarcon <facundoalarcon93@gmail.com>
2017-12-04 23:11:56 -0300

    programa Final se arreglo arriba.js

--- arriba.js
+++ arriba.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	});
 
 	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
+		if( $(this).scrollTop() >= 0 ){
 			$('.ir-arriba').slideDown(300);
 		} else {
 			$('.ir-arriba').slideUp(300);
